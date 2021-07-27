426aa35b9819531cd2af58df6a7269d2155ad641
Alexander Abnosov <abnosov@gmail.com>
2021-07-27 15:09:43 +0300

    removed 'Powered by Matrix' from auth footer

--- src/components/views/auth/VectorAuthFooter.tsx
+++ src/components/views/auth/VectorAuthFooter.tsx
@@ -36,12 +36,7 @@ const VectorAuthFooter = () => {
         );
     }
 
-    return (
-        <div className="mx_AuthFooter">
-            { authFooterLinks }
-            <a href="https://matrix.org" target="_blank" rel="noreferrer noopener">{ _t('Powered by Matrix') }</a>
-        </div>
-    );
+    return null;
 };
 
 VectorAuthFooter.replaces = 'AuthFooter';
